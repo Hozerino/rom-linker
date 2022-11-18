50e8e2383bec986f48f0826f15f0287d422ef8b7
GuiCoppini <guilhermecoppinip@gmail.com>
2022-11-18 01:18:14 -0300

    Add echo off

--- build_executables.bat
+++ build_executables.bat
@@ -1,2 +1,3 @@
+@echo off
 python setup.py py2exe
 pause
--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 import py2exe
+from common import print_cute_message
 
-print("Creating CLEAR executable")
+print_cute_message("# Creating CLEAR executable #")
 py2exe.freeze(
     options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
     console=['clear.py'],
     zipfile = None,
 )
-print("Successfully created CLEAR executable")
+print_cute_message("Successfull created CLEAR executable")
 print()
 
-print("Creating CREATE_LINKS executable")
+print_cute_message("Creating CREATE_LINKS executable")
 py2exe.freeze(
     options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
     console=['create_links.py'],
     zipfile = None,
 )
-print("Successfully created CREATE_LINKS executable")
+print_cute_message("Successfully created CREATE_LINKS executable")
